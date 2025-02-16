d460ba328e78ee5f10e957cb080d21c9e9c68e0c
makors <makors@muko.io>
2025-02-16 17:26:01 -0500

    fix citation tabs

--- components/CitationTabs.tsx
+++ components/CitationTabs.tsx
@@ -1,18 +1,19 @@
+
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { CitationInfo } from "./CitationInfo"
 import CitationTextarea from "./CitationTextarea"
 
-export default function CitationTabs() {    
+export default function CitationTabs() {
     return (
         <Tabs defaultValue="info" className="w-full">
             <TabsList className="grid w-full grid-cols-2">
                 <TabsTrigger value="info">Info</TabsTrigger>
                 <TabsTrigger value="citation">Citation</TabsTrigger>
             </TabsList>
-            <TabsContent value="info">
+            <TabsContent value="info" forceMount className="data-[state=inactive]:hidden">
                 <CitationInfo />
             </TabsContent>
-            <TabsContent value="citation">
+            <TabsContent value="citation" forceMount className="data-[state=inactive]:hidden">
                 <CitationTextarea />
             </TabsContent>
         </Tabs>
